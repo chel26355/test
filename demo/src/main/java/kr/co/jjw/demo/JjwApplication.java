d9453ffc0c8f4f03efb6d0fc46291658d63b6dc4
unknown <chel26355@naver.com>
2021-04-04 20:57:25 +0900

    first commit

---
+++ demo/src/main/java/kr/co/jjw/demo/JjwApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jjw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjwApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/kr/co/jjw/demo/JjwApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jjw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
